d436d1f08c09d27ceea17ce0059968fdd0f17f9e
joinej18 <106044374+joinej18@users.noreply.github.com>
2022-10-05 19:52:45 -0500

    Update tests.py
    tests update

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -2,16 +2,18 @@ import unittest
 
 from translator import english_to_french, french_to_english
 
-class Test_englishToFrench(unittest.TestCase):
-    def test1(self):
-        self.assertTrue('Hello', 'Bonjour')
-        self.assertTrue('bye', 'au revoir')
-        self.assertIsNotNone(self)
+class Test_englishtofrench(unittest.TestCase)
+   def teste2f(self)
+        self.assertNotEqual(english_to_french("None"), '')
+        self.assertNotEqual(english_to_french(0), 0)
+        self.assertEqual(english_to_french('Thank you'), 'Je vous remercie')
+        self.assertEqual(english_to_french('Goodbye'), 'Au revoir')
 
-class Test_frenchToEnglish(unittest.TestCase):
-    def test1(self):
-        self.assertTrue('Bonjour','Hello')
-        self.assertTrue('Au revoir','Bye')
-        self.assertIsNotNone(self)
+class Test_frenchToenglish(unittest.TestCase):
+   def testf2e(self):
+        self.assertNotEqual(french_to_english("None"), '')
+        self.assertNotEqual(french_to_english(0), 0)
+        self.assertEqual(french_to_english('Je vous remercie'), 'Thank you')
+        self.assertEqual(french_to_english('Au revoir'), 'Goodbye')
 
 unittest.main()
